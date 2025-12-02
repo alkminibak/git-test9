5af65dc795667e6bed73e38c27211802174bdfd7
alkminibak <alkmini.bak@gmail.com>
2025-12-02 20:52:32 +0200

    feat: say goodnight

--- src/gr/aueb/cf/testgit/SayGoodnight.java
+++ src/gr/aueb/cf/testgit/SayGoodnight.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.testgit;
 
 public class SayGoodnight {
     public static void main(String[] args) {
-        System.out.println("Good night!");
+        System.out.println("Good night!git");
     }
 }
